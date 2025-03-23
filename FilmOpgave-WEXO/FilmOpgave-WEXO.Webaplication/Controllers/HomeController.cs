5aa74481332470ce7b03c6e1d0810cd04c251f77
jimil1 <46991972+jimil1@users.noreply.github.com>
2025-03-23 10:17:21 +0100

    Almost working

--- FilmOpgave-WEXO/FilmOpgave-WEXO.Webaplication/Controllers/HomeController.cs
+++ FilmOpgave-WEXO/FilmOpgave-WEXO.Webaplication/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using FilmOpgave_WEXO.Webaplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using FilmOpgave_WEXO.Webaplication.Views
+using FilmOpgave_WEXO.Domain.Controllers;
 
 namespace FilmOpgave_WEXO.Webaplication.Controllers
 {
@@ -9,9 +9,15 @@ namespace FilmOpgave_WEXO.Webaplication.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly MovieController movieController;
+        private readonly GenreController genreController;
+
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            this.movieController = new MovieController();
+            this.genreController = new GenreController();
         }
 
         public IActionResult Index()
@@ -29,5 +35,51 @@ namespace FilmOpgave_WEXO.Webaplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        // Displays a list of genres.
+        public async Task<IActionResult> Genres()
+        {
+            var genres = await genreController.FetchGenresAsync();
+            var genreMovieCounts = new Dictionary<int, int>();
+
+            foreach (var genre in genres)
+            {
+                var movies = await movieController.FetchMoviesByGenreAsync(genre.Id);
+                genreMovieCounts[genre.Id] = movies.Count; // Store movie count for each genre
+            }
+
+            var viewModel = new GenreViewModel
+            {
+                Genres = genres,
+                GenreMovieCounts = genreMovieCounts
+            };
+
+            return View(viewModel);
+
+
+        }
+
+        // Displays movies by genre.
+        public async Task<IActionResult> MoviesByGenre(int genreId)
+        {
+            var movies = await movieController.FetchMoviesByGenreAsync(genreId);
+            return View(movies);
+        }
+
+        // Displays details of a movie by ID.
+        public async Task<IActionResult> MovieDetails(int movieId)
+        {
+            var movie = await movieController.FetchMovieAsync(movieId);
+            return View(movie);
+        }
+
+        // Returns a JSON list of genres.
+        public async Task<IActionResult> GenresJson()
+        {
+            var genres = await genreController.FetchGenresAsync();
+            return Json(genres);
+        }
+
+
     }
 }
---
+++ FilmOpgave-WEXO/FilmOpgave-WEXO.Webaplication/VeiwModels/GenreVeiwModel.cs
@@ -0,0 +1,11 @@
+﻿using FilmOpgave_WEXO.Domain.Models;
+
+namespace FilmOpgave_WEXO.Webaplication.VeiwModels
+
+{
+    public class GenreViewModel
+    {
+        public List<Genre> Genres { get; set; }
+        public Dictionary<int, int> GenreMovieCounts { get; set; }
+    }
+}
